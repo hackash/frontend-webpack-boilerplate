3508edfe21f0fc2bbe38e5443ef6cd4da807af92
Ashot <ashot.todo@gmail.com>
2018-12-19 12:05:33 +0400

    changed webpack config, added HTML plugin

--- src/js/app.js
+++
@@ -1,4 +0,0 @@
-/* The main JS file ES6 and jQuery are allowed here :) */
-
-/* Import styles here */
-import '../scss/app.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -1,83 +1,68 @@
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-const UglifyJSPlugin = require('webpack-uglifyes-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const webpack = require('webpack');
-
 const path = require('path');
+const mode = 'none';
 
-const config = {
+module.exports = {
+  mode: process.env.NODE_ENV || mode,
   entry: {
-    app: './src/js/app.js',
+    index: './src/scripts/index.js'
   },
   output: {
-    filename: 'js/[name].js',
     path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js'
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'postcss-loader', 'sass-loader'],
-        }),
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' }
+        ]
       },
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['es2015'],
-        },
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          }
+        }
       },
       {
-        test: /\.(png|gif|jpg|jpeg)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: { name: 'images/[name].[hash:6].[ext]', publicPath: '../', limit: 8192 },
-          },
-        ],
+        test: /\.html$/,
+        use: [{
+          loader: 'html-loader',
+          options: {
+            minimize: true
+          }
+        }]
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: { name: 'fonts/[name].[hash:6].[ext]', publicPath: '../', limit: 8192 },
-          },
-        ],
+        test: /\.(png|svg|jpg|gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]',
+            context: 'src'
+          }
+        }
       },
-    ],
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: {
+          loader: 'file-loader',
+        }
+      }
+    ]
   },
   plugins: [
-    new ExtractTextPlugin('css/[name].css'),
-    new BrowserSyncPlugin({
-      hash: false,
-      host: 'localhost',
-      port: 3000,
-      server: { baseDir: ['dist'] },
-    }),
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-    }),
     new HtmlWebpackPlugin({
       hash: false,
-      template: './src/index.html',
-      filename: './index.html',
-    }),
-  ],
+      template: './src/index.html'
+    })
+  ]
 };
-
-if (process.env.NODE_ENV === 'production') {
-  config.plugins.push(
-    new UglifyJSPlugin(),
-    new OptimizeCssAssetsPlugin(),
-  );
-}
-
-module.exports = config;
